72031babc31a03127a1ed476277d8efede0a9b17
CodeXRook <lacekitfly@gmail.com>
2019-03-23 10:34:13 -0400

    added port

--- index.js
+++ index.js
@@ -4,4 +4,9 @@ const app = express();
 app.get('/ping',(req, res) => {
     res.status(200);
     res.json({'pong': true});
+});
+
+const port = 5050
+app.listen(port,() =>{
+    console.log(`Listening on port ${port}`);
 })
