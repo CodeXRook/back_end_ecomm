0564b0fb7b97be9aac08bb72d404b75546b3ac4e
CodeXRook <lacekitfly@gmail.com>
2019-03-24 18:31:01 -0400

    updated status orders

--- seed.sql
+++ seed.sql
@@ -56,4 +56,6 @@ INSERT INTO products (id, name, shop_id, price, description, category, url) VALU
 (13,'jacab&co', 3, 1600, 'macbookpro', 'electronics', 'wwww.boxplug.com');
 
 INSERT INTO orders (id, user_id, order_total, order_status) VALUES
-(01, 22, 1, '65', 'pending');
+(01, 1, 5, 'pending'),
+(08, 3, 34, 'delivered'),
+(05, 2, 5, 'in tranist');
