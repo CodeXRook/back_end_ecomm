78ae3f154061215a1460ddd53c54a1b27f635af9
CodeXRook <lacekitfly@gmail.com>
2019-03-24 16:34:06 -0400

    updatedpost

--- routes/user.js
+++ routes/user.js
@@ -5,5 +5,8 @@ const userService = require('../service/user');
 // POST - CREATE
 userRouter.post('/',(req, res, next) =>{
     const {id, firstname, lastname, address, state, zipcode} = req.body;
+
+    
 })
+
 
