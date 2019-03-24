b9475a21d2c1f71f9a95d5c93d925ebab3512a6a
CodeXRook <lacekitfly@gmail.com>
2019-03-24 19:01:25 -0400

    Updated Services

--- routes/products.js
+++ routes/products.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const productsRouter = express.Router();
-const productRouter = require('../services/products');
+const ProductRouter = require('../services/products');
 
 // POST - CREATE
 productRouter.post('/', (req, res, next) => {
     const {id, name, shop_id, price, description, category, url} = req.body;
 
+    ProductService.create(id, name, shop_id, price, description, category, url)
+    .then(data => {
+        res.json({success: `Created Porduct named ${name} with generated ID: ${data.id}`});
+    })
 })
