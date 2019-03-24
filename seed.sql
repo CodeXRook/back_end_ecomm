ed818d3dc479d8fefef9347e31966f0279104e6d
CodeXRook <lacekitfly@gmail.com>
2019-03-24 18:10:09 -0400

    added products in table

--- seed.sql
+++ seed.sql
@@ -16,7 +16,8 @@ CREATE TABLE shops (
 id SERIAL PRIMARY KEY, 
 shopname VARCHAR NOT NULL,
 user_id INT REFERENCES users(id) NOT NULL,
-product_id INT REFERENCES products(id) NOT NULL,
+category VARCHAR NOT NULL,
+ address VARCHAR NOT NULL,
 state VARCHAR NOT NULL,
 city VARCHAR NOT NULL,
 zipcode VARCHAR NOT NULL
@@ -40,9 +41,13 @@ order_status VARCHAR NOT NULL
 );
 
 INSERT INTO users (firstname, lastname, address, state, zipcode) VALUES
-('Ash', 'Gary', '146 west st', 'New york', 10472),
-('Pat', 'Ewin', '55 bowery st', 'New york', 10162),
-('Mark', 'James', '123 main st', 'Main', 10563);
+('Ash', 'Gary', '146 west st', 'New york', '10472'),
+('Pat', 'Ewin', '55 bowery st', 'New york', '10162'),
+('Mark', 'James', '123 main st', 'Main', '10563');
 
-INSERT INTO shops (shopname, products, address, state, zipcode) VALUES
-('JeanSpace, Jeans, 34 howard st, New york, 10101')
+INSERT INTO shops (user_id, shopname, category, address, state, city, zipcode) VALUES
+(1,'JeanSpace', 'Clothing', '34 howard st', 'New york', 'Queens', '10101'),
+(3,'BoxPlug', 'electronics', '6 main st', 'New york','Bronx', '10101'),
+(2,'Jewel Bug', 'jewelry', '216 flabush ave','New york','Manhattan', '10493');
+
+INSERT INTO products (id, name, shop_id, price, description, category, url) VALUES
