b8093ab1698e20c2b1c780cf27a30a87a496deec
CodeXRook <lacekitfly@gmail.com>
2019-03-24 15:29:24 -0400

    new index

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const app = express();
 //FORWARD SLASH PING OR ANY TYPE TO ACCESS EXACT LOCAL HOST FILE
 app.get('/ping',(req, res) => {
     res.status(200);
-    res.json({'Text Work': true});
+    res.json({'Text Work':'🔥🔥🔥'});
 });
 
-const port = 5050
+const port = 5051
 app.listen(port,() =>{
     console.log(`Listening on port ${port}`);
 })
