205d30f10b74813cc5697fcf1ecfbb87c9f4b657
CodeXRook <lacekitfly@gmail.com>
2019-03-24 18:57:33 -0400

    items in Router

--- routes/products.js
+++ routes/products.js
@@ -3,3 +3,7 @@ const productsRouter = express.Router();
 const productRouter = require('../services/products');
 
 // POST - CREATE
+productRouter.post('/', (req, res, next) => {
+    const {id, name, shop_id, price, description, category, url} = req.body;
+
+})
