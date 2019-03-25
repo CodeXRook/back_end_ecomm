d266031668f1e2e613954421be10cf9965fb752e
CodeXRook <lacekitfly@gmail.com>
2019-03-25 18:35:06 -0400

    created sql products services

--- services/products.js
+++ services/products.js
@@ -1,2 +1,8 @@
 const {db} = require('./dbConnect');
 const ProductsServices = {};
+
+ProductsServices.create = (id, name, shop_id, price, description, category, url) =>{
+    const sql =`
+    INSERT INTO products (id, name, shop_id, price, description, category, url) VALUES
+    ($[id], $[name], $[shop_id], $[price], $[description], $[category], $[url]) RETURNING id;`;
+}
