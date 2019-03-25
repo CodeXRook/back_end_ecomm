7e3d1fde93d3ef8883d53b9bf59924620a4053da
CodeXRook <lacekitfly@gmail.com>
2019-03-25 19:20:31 -0400

    updating read

--- services/user.js
+++ services/user.js
@@ -3,5 +3,10 @@ const UserService ={};
 
 UserService.create = (id, user_id, order_total, order_status) =>{
     const sql = `INSERT INTO user (id, user_id, order_total, order_status) VALUES ($[id], $[user_id], $[order_total], $[order_status]) RETURNING id`;
-    
+    return db.one(sql, {id, user_id, order_total, order_status});
+}
+
+UserService.read =(id) => {
+    const sql =`
+    `
 }
