26de85bf7e548563139bef6d66f01a7ddf3b34f9
CodeXRook <lacekitfly@gmail.com>
2019-03-25 18:44:46 -0400

    added read services

--- services/products.js
+++ services/products.js
@@ -5,4 +5,19 @@ ProductsServices.create = (id, name, shop_id, price, description, category, url)
     const sql =`
     INSERT INTO products (id, name, shop_id, price, description, category, url) VALUES
     ($[id], $[name], $[shop_id], $[price], $[description], $[category], $[url]) RETURNING id;`;
+    return db.one(sql, {id, name, shop_id, price, description, category, url});
+}
+
+ProductsServices.read = (id) => {
+    const sql =`
+    SELECT
+    products.*
+    id.name AS id_name
+    FROM products
+    JOIN id
+    ON products.id_id = id.id
+    WHERE
+    products.id =$[id]
+    `;
+    return db.one(sql, {id});
 }
