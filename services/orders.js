ded57f655cc73f233103c8fcfe7434612f9f7893
CodeXRook <lacekitfly@gmail.com>
2019-03-25 20:32:30 -0400

    updating orders

--- services/orders.js
+++ services/orders.js
@@ -2,5 +2,5 @@ const {db} = require('./dbConnect');
 const OrdersService ={};
 
 OrdersService.create =(id, user_id, order_total, order_status) => {
-    
+    const sql = `INSERT INTO orders (id, user_id, order_total, order_status) VALUES ($[id], $[user_id])`
 }
