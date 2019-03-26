05ec695b555c823491a156eb143e04dff88cb12b
CodeXRook <lacekitfly@gmail.com>
2019-03-25 20:34:37 -0400

    setting up order create

--- services/orders.js
+++ services/orders.js
@@ -2,5 +2,13 @@ const {db} = require('./dbConnect');
 const OrdersService ={};
 
 OrdersService.create =(id, user_id, order_total, order_status) => {
-    const sql = `INSERT INTO orders (id, user_id, order_total, order_status) VALUES ($[id], $[user_id])`
+    const sql = `INSERT INTO orders (id, user_id, order_total, order_status) VALUES ($[id], $[user_id])`;
+    return db.one(sql, {id, user_id, order_total, order_status});
+}
+
+OrdersService.read = (id) => {
+const sql = `
+SELECT
+order
+`
 }
