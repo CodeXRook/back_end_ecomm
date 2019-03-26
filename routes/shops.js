a3a023857b012f9f176ec366f67bd81e0cc87758
CodeXRook <lacekitfly@gmail.com>
2019-03-26 02:42:01 -0400

    updated shop servies

--- routes/shops.js
+++ routes/shops.js
@@ -1,3 +1,13 @@
 const express = require('express');
 const shopsRouter =express.Router();
 const ShopsService = require('../services/shops');
+
+//POST -- CREATE
+shopsRouter.post('/', (req, rew, next) => {
+    const {user_id, shopname, category, address, state, city, zipcode};
+
+    ShopsService.create(user_id, shopname, category, address, state, city, zipcode)
+    .then(data => {
+        
+    })
+})
