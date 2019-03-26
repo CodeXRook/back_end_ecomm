6ce9da81a532667b6e3cf597c22f926ba2095a1b
CodeXRook <lacekitfly@gmail.com>
2019-03-26 02:20:14 -0400

    updated get READ

--- routes/orders.js
+++ routes/orders.js
@@ -5,4 +5,22 @@ const OrdersService = require('../service/orders');
 //POST- CREATE
 OrdersRouter.post('/',(req, res, next) => {
    const {id, user_id, order_total, order_status} = res.body;
+
+   OrdersRouter.Services.create(id, user_id, order_total, order_status)
+   .then(data =>{
+       res.json({success: `Created Orders user_id ${user_id} with generated ID: ${data.id}`});
+   })
+   .catch(err => {
+       next(err);
+   })
+});
+
+//GET PAID
+ordersRouter.get('/:id/', (req, res. next) =>{
+    const {id} = req.param;
+
+    OrdersService.read(id)
+    .then(data => {
+        res.json(data);
+    })
 })
