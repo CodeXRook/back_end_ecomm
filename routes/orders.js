2ccc64c9d28064da1bf11989ed56f01d99848fb0
CodeXRook <lacekitfly@gmail.com>
2019-03-26 02:07:51 -0400

    updated post in routes

--- routes/orders.js
+++ routes/orders.js
@@ -1,2 +1,8 @@
-const express =require('express');
-const ordersRouter =express.Router();
+const express = require('express');
+const OrdersRouter = express.Router();
+const OrdersService = require('../service/orders');
+
+//POST- CREATE
+OrdersRouter.post('/',(req, res, next) => {
+   const {id, user_id, order_total, order_status} = res.body;
+})
